515e248049b7928972ddbc383d88305d89824a82
decentsimple <decentsimple@yahoo.com.ph>
2022-11-14 04:43:54 +0800

    1st commit to java repository in Git

---
+++ project1/src/practice/Array.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class Array {
+	public static String[] colors = {"yellow", "white"};
+
+	public static void main(String[] args) {
+		System.out.println(colors.length);
+	}
+}
