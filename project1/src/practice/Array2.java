1fa15df30e5e6b9066c72dd6bcecf5895ad77fca
decentsimple <decentsimple@yahoo.com.ph>
2022-11-14 05:22:41 +0800

    adding array2 class

---
+++ project1/src/practice/Array2.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Array2 {
+	public static void main(String[] args) {
+		int[] randomNumbers = {51, 108, 28};
+		for (int number : randomNumbers) {
+			if (number > 80) {
+				System.out.println("This is higher than 80: " + number);
+			} else if (number > 27) {
+				System.out.println("this is higher than 27: " + number);
+			} else {
+				System.out.println("just print this out: " + number);
+			}
+		}
+	}
+}
